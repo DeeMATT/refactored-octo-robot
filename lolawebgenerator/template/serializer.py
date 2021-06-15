be3aa0eabfaa8196cc59266983846c5e3e4737d2
DeeMATT <mattode@outlook.com>
2021-06-15 21:11:05 +0100

    TemplateSerializer updated

--- lolawebgenerator/template/serializer.py
+++ lolawebgenerator/template/serializer.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 class TemplateSerializer(serializers.Serializer):
     template_files = serializers.FileField(required=True, write_only=True)
+    template_screen = serializers.FileField(required=True, write_only=True)
     templateName = serializers.CharField(read_only=True)
     template_url = serializers.CharField(read_only=True)
 
