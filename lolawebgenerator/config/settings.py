554f7aff20ad8c16a2176f504f19dcd708bd83c8
DeeMATT <mattode@outlook.com>
2021-06-18 17:18:43 +0100

    update

--- lolawebgenerator/config/settings.py
+++ lolawebgenerator/config/settings.py
@@ -130,10 +130,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    '*'
+    'http://localhost:3000'
     ]
 
-# http://localhost:3000
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = False
 
