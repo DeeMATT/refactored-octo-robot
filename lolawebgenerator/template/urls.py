9fdc59230fff78b4b08b390335841ba5df397566
DeeMATT <mattode@outlook.com>
2021-06-19 20:25:53 +0100

    url update

--- lolawebgenerator/template/urls.py
+++ lolawebgenerator/template/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", templates, name="list.create.templates"),
     path("/<int:id>/dataspec", template_dataspec, name='template.dataspec'),
     path("/<int:id>/process", process_template, name='process.template'),
-    path("/upload-template", upload_processed_template)
+    path("/upload-template/<str:domain>", upload_processed_template)
 ]
