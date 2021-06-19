d85b11baa6715da6756d87b71f50dcbd7d0d4a47
DeeMATT <mattode@outlook.com>
2021-06-19 18:18:21 +0100

    "/upload-template" endpoint

--- lolawebgenerator/template/urls.py
+++ lolawebgenerator/template/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from template.views import templates, template_detaspec, process_template
+from template.views import templates, template_dataspec, process_template, upload_processed_template
 
 urlpatterns = [
     path("", templates, name="list.create.templates"),
-    path("/<int:id>/dataspec", template_detaspec, name='template.dataspec'),
-    path("/<int:id>/process", process_template, name='process.template')
-
+    path("/<int:id>/dataspec", template_dataspec, name='template.dataspec'),
+    path("/<int:id>/process", process_template, name='process.template'),
+    path("/upload-template", upload_processed_template)
 ]
