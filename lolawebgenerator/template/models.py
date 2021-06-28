674666fe92c63d70288a7ddc6d7b5c1cb21cdfa1
DeeMATT <mattode@outlook.com>
2021-06-28 11:30:27 +0100

    screenshot removed

--- lolawebgenerator/template/models.py
+++ lolawebgenerator/template/models.py
@@ -4,4 +4,3 @@ from django.db import models
 class Template(models.Model):
     name = models.CharField(blank=False, null=False, max_length=500)
     unique_name = models.CharField(blank=False, null=False, max_length=500)
-    screenshot = models.CharField(blank=False, null=True, max_length=500)
